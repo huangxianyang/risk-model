a3d78c9ee394666566292fac46e4bf434192e5da
huangsir <hxysir@163.com>
2020-02-07 15:37:30 +0800

    更新setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     keywords = ["pip", "EDA","Preprocessing", "FeatureEngineering", "Modeling","RiskStragety"],
 
     packages = find_packages(),# 所有包含__init__.py文件的目录
-    package_data={'riskscore':['data/*.csv','data/*.txt']},
+    package_data={'riskModel':['data/*.csv','data/*.txt']},
     # include_package_data = True,
     install_requires = ['pymongo>=3.7.2','PyMySQL>=0.9.3','missingno>=0.4.1','pandas-profiling>=1.4.1','numpy>=1.15.0',
                         'matplotlib>=3.0.2','pandas>=0.23.4','scikit-learn>=0.21.3','imbalanced-learn==0.5.0',
